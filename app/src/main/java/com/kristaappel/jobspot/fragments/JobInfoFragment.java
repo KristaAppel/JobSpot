f6f97beb1cf937cf988af7b8a415a3606f837f1d
kristaappel <mrskrista85@gmail.com>
2017-09-30 12:56:56 -0400

    Jobs can be saved/unsaved from the job info screen

--- app/src/main/java/com/kristaappel/jobspot/fragments/JobInfoFragment.java
+++ app/src/main/java/com/kristaappel/jobspot/fragments/JobInfoFragment.java
@@ -13,8 +13,16 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.firebase.client.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.kristaappel.jobspot.R;
+import com.kristaappel.jobspot.objects.FileUtil;
 import com.kristaappel.jobspot.objects.Job;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -85,6 +93,16 @@ public class JobInfoFragment extends android.app.Fragment implements View.OnClic
         sharebutton.setOnClickListener(this);
         saveButton.setOnClickListener(this);
         appliedButton.setOnClickListener(this);
+
+        // Find out if job is saved:
+        ArrayList<Job> savedJobs = FileUtil.readSavedJobs(getActivity());
+        for (Job savedJob : savedJobs){
+            if (savedJob.getJobID().equals(job.getJobID())){
+                // The job is saved, so display filled star:
+                saveButton.setImageResource(R.drawable.ic_star_saved);
+                saveButton.setTag(R.drawable.ic_star_saved);
+            }
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -121,6 +139,46 @@ public class JobInfoFragment extends android.app.Fragment implements View.OnClic
                 Log.i("JobInfoFragment", "share the job");
                 break;
             case R.id.jobInfo_button_save:
+                Firebase firebase = new Firebase("https://jobspot-a0171.firebaseio.com/");
+                FirebaseAuth mAuth = FirebaseAuth.getInstance();
+                FirebaseUser firebaseUser = mAuth.getCurrentUser();
+                ImageButton saveButton = (ImageButton) v.findViewById(R.id.jobInfo_button_save);
+
+                // If the job is not saved, then save it:
+                if ((Integer)saveButton.getTag() == R.drawable.ic_star_unsaved){
+                    saveButton.setImageResource(R.drawable.ic_star_saved);
+                    saveButton.setTag(R.drawable.ic_star_saved);
+                    // Save the job to the device:
+                    ArrayList<Job> savedJobs = FileUtil.readSavedJobs(getActivity());
+                    savedJobs.add(job);
+                    FileUtil.writeSavedJob(getActivity(), savedJobs);
+                    // Save the job to Firebase:
+                    if (firebaseUser != null) {
+                        firebase.child("users").child(firebaseUser.getUid()).child("savedjobs").child(job.getJobID()).setValue(job);
+                    }
+                    Toast.makeText(getActivity(), "Job has been saved.", Toast.LENGTH_SHORT).show();
+
+                    // If the job is saved, then unsave it:
+                }else if ((Integer)saveButton.getTag() == R.drawable.ic_star_saved){
+                    saveButton.setImageResource(R.drawable.ic_star_unsaved);
+                    saveButton.setTag(R.drawable.ic_star_unsaved);
+                    // Unsave the job from the device:
+                    ArrayList<Job> savedJobs = FileUtil.readSavedJobs(getActivity());
+                    ArrayList<Job> jobsToRemove = new ArrayList<Job>();
+                    for (Job savedJob : savedJobs){
+                        if (savedJob.getJobID().equals(job.getJobID())){
+                            jobsToRemove.add(savedJob);
+                        }
+                    }
+                    savedJobs.removeAll(jobsToRemove);
+                    FileUtil.writeSavedJob(getActivity(), savedJobs);
+                    // Unsave the job from Firebase:
+                    if (firebaseUser != null) {
+                        firebase.child("users").child(firebaseUser.getUid()).child("savedjobs").child(job.getJobID()).removeValue();
+                    }
+                    Toast.makeText(getActivity(), "Job has been removed.", Toast.LENGTH_SHORT).show();
+                }
+
                 //TODO: save job to device & firebase, change icon to red star
                 Log.i("JobInfoFragment", "save the job");
                 break;
