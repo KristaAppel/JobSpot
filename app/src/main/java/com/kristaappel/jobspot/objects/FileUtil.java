53b86596e707797db71de97fff7d9c8ac4bdbc00
kristaappel <mrskrista85@gmail.com>
2017-10-02 09:45:39 -0400

    Saved & Applied Jobs are saved to the device on a separate file for each user

--- app/src/main/java/com/kristaappel/jobspot/objects/FileUtil.java
+++ app/src/main/java/com/kristaappel/jobspot/objects/FileUtil.java
@@ -2,6 +2,11 @@ package com.kristaappel.jobspot.objects;
 
 
 import android.content.Context;
+
+import com.firebase.client.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
@@ -10,10 +15,29 @@ import java.util.ArrayList;
 
 public class FileUtil {
 
+    private static String fileTypeSaved = "saved";
+    private static String fileTypeApplied = "applied";
+
+    private static String getFileName(String fileType){
+        Firebase firebase = new Firebase("https://jobspot-a0171.firebaseio.com/");
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = mAuth.getCurrentUser();
+        String savedJobsFile = "savedjobs" + firebaseUser.getUid() + ".txt";
+        String appliedJobsFile = "appliedjobs" + firebaseUser.getUid() + ".txt";
+        if (fileType.equals(fileTypeSaved)){
+            return savedJobsFile;
+        }else if (fileType.equals(fileTypeApplied)){
+            return appliedJobsFile;
+        }else {
+            return null;
+        }
+    }
+
     public static void writeSavedJob(Context context, ArrayList<Job> savedJobs) {
         try {
             // Save the arraylist to local storage:
-            FileOutputStream fileOutputStream = context.openFileOutput("savedjobs.txt", Context.MODE_PRIVATE);
+            String fileName = getFileName(fileTypeSaved);
+            FileOutputStream fileOutputStream = context.openFileOutput(fileName, Context.MODE_PRIVATE);
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
             objectOutputStream.writeObject(savedJobs);
             objectOutputStream.close();
@@ -25,7 +49,8 @@ public class FileUtil {
     public static void writeAppliedJob(Context context, ArrayList<Job> appliedJobs) {
         try {
             // Save the arraylist to local storage:
-            FileOutputStream fileOutputStream = context.openFileOutput("appliedjobs.txt", Context.MODE_PRIVATE);
+            String fileName = getFileName(fileTypeApplied);
+            FileOutputStream fileOutputStream = context.openFileOutput(fileName, Context.MODE_PRIVATE);
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
             objectOutputStream.writeObject(appliedJobs);
             objectOutputStream.close();
@@ -38,7 +63,8 @@ public class FileUtil {
         ArrayList<Job> savedJobs = new ArrayList<>();
         try {
             // Read from local storage:
-            FileInputStream fileInputStream = context.openFileInput("savedjobs.txt");
+            String fileName = getFileName(fileTypeSaved);
+            FileInputStream fileInputStream = context.openFileInput(fileName);
             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
             Object readObject = objectInputStream.readObject();
 
@@ -66,7 +92,8 @@ public class FileUtil {
         ArrayList<Job> appliedJobs = new ArrayList<>();
         try {
             // Read from local storage:
-            FileInputStream fileInputStream = context.openFileInput("appliedjobs.txt");
+            String fileName = getFileName(fileTypeApplied);
+            FileInputStream fileInputStream = context.openFileInput(fileName);
             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
             Object readObject = objectInputStream.readObject();
 
