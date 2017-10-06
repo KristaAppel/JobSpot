e701c643c23d758118279aa42f8e84f565ff2d2c
kristaappel <mrskrista85@gmail.com>
2017-10-06 17:02:22 -0400

    Implemented notifications

--- app/src/main/java/com/kristaappel/jobspot/objects/NotificationBroadcastReceiver.java
+++ app/src/main/java/com/kristaappel/jobspot/objects/NotificationBroadcastReceiver.java
@@ -1,17 +1,25 @@
 package com.kristaappel.jobspot.objects;
 
 import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.icu.util.Calendar;
-import android.provider.Settings;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.WakefulBroadcastReceiver;
 import android.util.Log;
 
+import com.kristaappel.jobspot.BottomNavigationActivity;
+import com.kristaappel.jobspot.R;
+
 import java.util.Date;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class NotificationBroadcastReceiver extends WakefulBroadcastReceiver {
 
@@ -21,7 +29,7 @@ public class NotificationBroadcastReceiver extends WakefulBroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Log.i("Receiver", "onReceive");
         setAlarm(context);
-        //TODO: make/send notification now
+        showNotification(context);
     }
 
     public void setAlarm(Context context){
@@ -32,7 +40,7 @@ public class NotificationBroadcastReceiver extends WakefulBroadcastReceiver {
 
         java.util.Calendar calendar = java.util.Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.add(java.util.Calendar.SECOND, 10); //TODO: change this to 24 hours
+        calendar.add(java.util.Calendar.SECOND, 24); //TODO: change this to 24 hours
         Date date = calendar.getTime();
 
         alarmManager.setExact(AlarmManager.RTC_WAKEUP, date.getTime(), pendingIntent);
@@ -52,6 +60,32 @@ public class NotificationBroadcastReceiver extends WakefulBroadcastReceiver {
         ComponentName receiver = new ComponentName(context, BootReceiver.class);
         PackageManager packageManager = context.getPackageManager();
         packageManager.setComponentEnabledSetting(receiver, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+    }
+
+    public void showNotification(Context context){
+            // Create an expanded notification
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+            builder.setSmallIcon(R.drawable.jobspot_small_notification_icon);
+            builder.setContentTitle("New Jobs");
+            builder.setContentText("There are new jobs waiting for you!");
+            Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.jobspot_large_notification_icon);
+            builder.setLargeIcon(bitmap);
+
+
+            // Create PendingIntent to open the app:
+            Intent intent = new Intent(context, BottomNavigationActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            // Tell the notification to call the PendingIntent when the notification is clicked:
+            builder.setContentIntent(pendingIntent);
+            // Tell the notification to cancel when it is clicked:
+            builder.setAutoCancel(true);
+
+            Notification notification = builder.build();
+
+            // Show the notification:
+            NotificationManager manager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+            manager.notify(0x0003, notification);
     }
 
 
